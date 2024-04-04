ed9c4cccb356ef0caedcb902dc7e555d96a49984
Gin <ginggeli@hotmail.com>
2024-04-04 11:20:17 +0200

    Create urls.py

---
+++ Bolar/Accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from Accounts.views import LoginView, RegisterView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
+
+app_name='Accounts'
+
+urlpatterns = [
+    path('', views.account, name='account'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    
+    #path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    #path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    #path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
